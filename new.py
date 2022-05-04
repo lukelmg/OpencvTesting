519fc6fba14e5f5d00a1f6902251a886307098dc
lukelmg <lukegutman14@gmail.com>
2022-05-04 12:52:56 -0400

    really good stuff

--- new.py
+++ new.py
@@ -4,6 +4,7 @@ cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
 while True:
     _, frame = cap.read()
+    _, box = cap.read()
     blurred_frame = cv2.GaussianBlur(frame, (5, 5), 0)
     hsv = cv2.cvtColor(blurred_frame, cv2.COLOR_BGR2HSV)
 
@@ -14,10 +15,18 @@ while True:
     contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
     for contour in contours:
-        cv2.drawContours(frame, contour, -1, (0, 0, 255), 3)
+        cv2.drawContours(frame, contour, -1, (255, 0, 0), 3)
+
+    if len(contours) != 0:
+        for mask_contour in contours:
+            if cv2.contourArea(mask_contour) > 500:
+                x, y, w, h = cv2.boundingRect(mask_contour)
+                cv2.rectangle(box, (x, y), (x + w, y + h), (0, 0, 255), 3) #drawing rectangle
+
 
     cv2.imshow("Frame", frame)
     cv2.imshow("Mask", mask)
+    cv2.imshow("Outline", box)
     key = cv2.waitKey(1)
     if key == 27:
         break
